3e6fd7bc20fdd121f0ad687a72aae4ba93215da4
nelio12 <57465206+nelio12@users.noreply.github.com>
2023-04-12 20:26:17 +0200

    Update index.js

--- ui/src/i18n/index.js
+++ ui/src/i18n/index.js
@@ -18,13 +18,14 @@ const languageList = {
   fr: 'French (fr)',
   th: 'Thai (th)',
   es: 'Spanish (es)',
+  ca: 'Catalan (ca)',
 };
 
 /**
  * Update this array each time a new translation is added
  * It must be a valid language shortcut, please see languages.js for available shortcuts
  */
-const languages = ['de', 'nl', 'en', 'fr', 'th', 'es'];
+const languages = ['de', 'nl', 'en', 'fr', 'th', 'es', 'ca'];
 
 const supportedLanguages = (lang) => {
   if (!lang) {
